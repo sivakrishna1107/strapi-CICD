ce344e8177f8e64147d319d53623a03429101d57
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-17 20:41:24 +0530

    Configure ECS cluster and service for Strapi
    Added ECS cluster, IAM role, and service for Strapi using Fargate.

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,7 +1,33 @@
+resource "aws_ecs_cluster" "this" {
+  name = "strapi-cluster"
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
-  requires_compatibilities = ["EC2"]
-  network_mode             = "bridge"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
@@ -22,3 +48,16 @@ resource "aws_ecs_task_definition" "strapi" {
   ])
 }
 
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.public.id]
+    security_groups = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+}
