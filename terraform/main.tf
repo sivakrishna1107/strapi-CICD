1f4a07cb1d988f267e390e42bc33fd02a5f0ba70
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-17 20:29:14 +0530

    Delete Strapi ECS task definition from main.tf
    Removed the ECS task definition resource for Strapi.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,27 +21,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "strapi"
-      image     = "${var.ecr_repository_url}:${var.image_tag}"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 1337
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-}
-
