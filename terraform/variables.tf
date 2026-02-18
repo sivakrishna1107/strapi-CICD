b1a66737bbf14fdb1edbb4a19b3aa91f312beb4e
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-18 21:30:10 +0530

    Add default value for execution_role_arn variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "execution_role_arn" {
   description = "Existing ECS task execution role ARN"
   type        = string
+  default     = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole"
 }
 
